dbf077cb65d8044593c7b679dd1ffc47163841e7
mukesh <44917136+namballa-mukesh@users.noreply.github.com>
2020-02-27 17:27:55 +0530

    Add files via upload

---
+++ profiles/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Comment
+
+
+class CommentForm(forms.ModelForm):
+    content = forms.CharField(label="", widget=forms.Textarea(
+        attrs={'class': 'form-control', 'placeholder': 'Text goes here!', 'rows': '4', 'cols': '50'}))
+
+    class Meta:
+        model = Comment
+        fields = ('content',)
